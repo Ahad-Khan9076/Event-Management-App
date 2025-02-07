bb5887c7e30616309de3a748cc43985e2ff4a8a9
Ahad Ali khan <ahadalikhan1525@gmail.com>
2025-02-07 21:04:53 +0500

    Initial commit

---
+++ lib/views/splash/splash_pagee.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashPagee extends StatefulWidget {
+  const SplashPagee({super.key});
+
+  @override
+  State<SplashPagee> createState() => _SplashPageeState();
+}
+
+class _SplashPageeState extends State<SplashPagee> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
